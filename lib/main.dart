68bce4dd3e90f9a834530c899ccaa4e80f106e23
Anoop M <anoopm6@gmail.com>
2020-07-05 16:37:47 +0200

    Update provider to provider: ^4.1.3+1

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: ChangeNotifierProvider<MQTTAppState>(
-        builder: (_) => MQTTAppState(),
+        create: (_) => MQTTAppState(),
         child: MQTTView(),
       )
     );
